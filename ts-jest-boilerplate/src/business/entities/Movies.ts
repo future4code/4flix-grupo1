c5b527f05d798b0e221b8b34575fb049e56716d7
Brunna Rocha <brunna.17rocha03@gmail.com>
2020-01-09 12:04:30 -0300

    Entidade Movies criada

---
+++ ts-jest-boilerplate/src/business/entities/Movies.ts
@@ -0,0 +1,18 @@
+export class Movies {
+    private title: string
+    private date: string
+    private length: string
+    private synopsis: string
+    private link: string
+    private picture: string
+
+    constructor(
+        title:string,
+        date: string,
+        length: string,
+        synopsis: string,
+        link: string,
+        picture: string
+    ) {}
+}
+
